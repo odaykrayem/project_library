848be95bbe88a417015390a57b9d070292876632
saadfattouh <saadfattoh98@gmail.com>
2021-05-07 23:01:05 +0300

    2nd commit

---
+++ app/src/main/java/com/digitalminds/projectlibrary/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.digitalminds.projectlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //for changing status bar icon colors
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.login_bk_color));
+        }
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginClick(View View){
+        startActivity(new Intent(this,RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
+    }
+
+}
